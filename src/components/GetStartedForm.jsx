35ffb0587d6a3091aa7605050436bca6a0169ad5
mdhasnainhanif <hasmdnain39@gmail.com>
2024-04-25 13:22:24 +0500

    faq section done

--- src/components/GetStartedForm.jsx
+++ src/components/GetStartedForm.jsx
@@ -5,10 +5,10 @@ import { FaAngleRight } from "react-icons/fa6";
 const GetStartedForm = () => {
   return (
     <>
-        <div className="w-3/4">
+        <div className="">
             <input type="search" placeholder="Email Address" className="text-white mt-4 bg-[#191917] py-3 px-4 md:w-[30%] border border-slate-400 rounded" />
             <Button name="Get Started" class="bg-[#C11119] text-white py-3 px-7 ms-3 md:mt-0 mt-3 text-[18px]" icon={<FaAngleRight className="inline mb-1" />} />
-          </div>
+        </div>
     </>
   )
 }
